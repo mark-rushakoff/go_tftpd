f710b8300e34bc7bdb5db648cfc39bf85148b63c
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-08-19 13:19:03 -0700

    Add safe read/write packets

---
+++ safe_packets/safe_read_write.go
@@ -0,0 +1,28 @@
+package safe_packets
+
+import ()
+
+type ReadWriteMode string
+
+const (
+	NetAscii ReadWriteMode = "netascii"
+	Octet    ReadWriteMode = "octet"
+)
+
+type SafeReadRequest struct {
+	Filename string
+	Mode     ReadWriteMode
+}
+
+type SafeWriteRequest struct {
+	Filename string
+	Mode     ReadWriteMode
+}
+
+func NewSafeReadRequest(filename string, mode ReadWriteMode) *SafeReadRequest {
+	return &SafeReadRequest{filename, mode}
+}
+
+func NewSafeWriteRequest(filename string, mode ReadWriteMode) *SafeWriteRequest {
+	return &SafeWriteRequest{filename, mode}
+}
