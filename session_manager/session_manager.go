da1556a567ceaa9346befc2689bccbcedf08bfbf
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-08-31 23:56:38 -0700

    Update session manager

--- session_manager/session_manager.go
+++ session_manager/session_manager.go
@@ -29,36 +29,19 @@ func NewSessionManager(readSessionFactory read_session.ReadSessionFactory) *Sess
 	}
 }
 
-// Block forever and handle requests from the associated SafetyFilter.
-func (m *SessionManager) Watch() {
-	for {
-		select {
-		case incomingReadRequest := <-m.ReadRequest:
-			m.makeReadSessionFromIncomingRequest(incomingReadRequest)
-		case incomingAck := <-m.Ack:
-			m.sendAckToReadSession(incomingAck)
-		}
-	}
-}
-
-func (m *SessionManager) makeReadSessionFromIncomingRequest(incomingReadRequest *safety_filter.IncomingSafeReadRequest) {
-	m.lock.Lock()
-	defer m.lock.Unlock()
+func (m *SessionManager) MakeReadSessionFromIncomingRequest(incomingReadRequest *safety_filter.IncomingSafeReadRequest) {
 	session := m.readSessionFactory(incomingReadRequest.Read.Filename, incomingReadRequest.Addr)
-	if session.Config == nil {
-		panic("made session with a nil config")
-	}
+	m.lock.Lock()
 	m.readSessions[sessionKey(incomingReadRequest.Addr)] = session
+	m.lock.Unlock()
 	session.Begin()
 }
 
-func (m *SessionManager) sendAckToReadSession(incomingAck *safety_filter.IncomingSafeAck) {
+func (m *SessionManager) SendAckToReadSession(incomingAck *safety_filter.IncomingSafeAck) {
 	m.lock.Lock()
-	defer m.lock.Unlock()
 	session := m.readSessions[sessionKey(incomingAck.Addr)]
-	go func() {
-		session.Ack <- incomingAck.Ack
-	}()
+	m.lock.Unlock()
+	session.Ack() <- incomingAck.Ack
 }
 
 func sessionKey(addr net.Addr) string {
--- session_manager/session_manager_test.go
+++ session_manager/session_manager_test.go
@@ -19,25 +19,24 @@ func TestIncomingReadCreatesSession(t *testing.T) {
 	factoryCalls := make(chan bool, 1)
 	readSessionFactory := func(filename string, _ net.Addr) *read_session.ReadSession {
 		factoryCalls <- true
-		return &read_session.ReadSession{
-			Config: &read_session.ReadSessionConfig{
+		return read_session.NewReadSession(
+			&read_session.ReadSessionConfig{
 				ResponseAgent:     response_agent.MakeMockResponseAgent(),
 				Reader:            strings.NewReader(filename),
 				BlockSize:         1,
 				TimeoutController: timeout_controller.MakeMockTimeoutController(),
 			},
-		}
+		)
 	}
 	manager := NewSessionManager(readSessionFactory)
-	go manager.Watch()
 
 	read := safe_packets.NewSafeReadRequest("foobar", safe_packets.NetAscii)
 	addr := test_helpers.MakeMockAddr("fakenet", "a")
 
-	manager.ReadRequest <- &safety_filter.IncomingSafeReadRequest{
+	go manager.MakeReadSessionFromIncomingRequest(&safety_filter.IncomingSafeReadRequest{
 		Read: read,
 		Addr: addr,
-	}
+	})
 
 	select {
 	case <-factoryCalls:
@@ -49,56 +48,62 @@ func TestIncomingReadCreatesSession(t *testing.T) {
 
 func TestIncomingAckIsRoutedToCorrectSession(t *testing.T) {
 	responseAgentA := response_agent.MakeMockResponseAgent()
-	readSessionA := &read_session.ReadSession{
-		Config: &read_session.ReadSessionConfig{
+	readSessionA := read_session.NewReadSession(
+		&read_session.ReadSessionConfig{
 			ResponseAgent:     responseAgentA,
 			Reader:            strings.NewReader("Abc"),
 			BlockSize:         1,
 			TimeoutController: timeout_controller.MakeMockTimeoutController(),
 		},
-		Ack: make(chan *safe_packets.SafeAck),
-	}
+	)
 
 	responseAgentB := response_agent.MakeMockResponseAgent()
-	readSessionB := &read_session.ReadSession{
-		Config: &read_session.ReadSessionConfig{
+	readSessionB := read_session.NewReadSession(
+		&read_session.ReadSessionConfig{
 			ResponseAgent:     responseAgentB,
 			Reader:            strings.NewReader("Def"),
 			BlockSize:         1,
 			TimeoutController: timeout_controller.MakeMockTimeoutController(),
 		},
-		Ack: make(chan *safe_packets.SafeAck),
-	}
+	)
+
 	totalCalls := 0
 	readSessionFactory := func(filename string, _ net.Addr) *read_session.ReadSession {
 		totalCalls++
 		if totalCalls == 1 {
+			println("Returning readSessionA")
 			return readSessionA
 		} else if totalCalls == 2 {
+			println("Returning readSessionB")
 			return readSessionB
 		} else {
 			panic("test Read session factory called too many times")
 		}
 	}
 	manager := NewSessionManager(readSessionFactory)
-	go manager.Watch()
 
-	manager.ReadRequest <- &safety_filter.IncomingSafeReadRequest{
+	go manager.MakeReadSessionFromIncomingRequest(&safety_filter.IncomingSafeReadRequest{
 		Read: safe_packets.NewSafeReadRequest("foobar", safe_packets.NetAscii),
 		Addr: test_helpers.MakeMockAddr("fakenet", "a"),
-	}
+	})
+
+	runtime.Gosched()
 
 	expectedData := safe_packets.NewSafeData(1, []byte("A"))
 	if !responseAgentA.MostRecentData().Equals(expectedData) {
 		t.Fatalf("Received incorrect data %v, expected %v", responseAgentA.MostRecentData(), expectedData)
 	}
 
-	manager.ReadRequest <- &safety_filter.IncomingSafeReadRequest{
+	go manager.MakeReadSessionFromIncomingRequest(&safety_filter.IncomingSafeReadRequest{
 		Read: safe_packets.NewSafeReadRequest("baz", safe_packets.NetAscii),
 		Addr: test_helpers.MakeMockAddr("fakenet", "b"),
-	}
+	})
 
 	runtime.Gosched()
+
+	if responseAgentB.MostRecentData() == nil {
+		panic("nil data?!")
+	}
 
 	expectedData = safe_packets.NewSafeData(1, []byte("D"))
 	if !responseAgentB.MostRecentData().Equals(expectedData) {
@@ -108,12 +113,10 @@ func TestIncomingAckIsRoutedToCorrectSession(t *testing.T) {
 	responseAgentA.Reset()
 	responseAgentB.Reset()
 
-	go func() {
-		manager.Ack <- &safety_filter.IncomingSafeAck{
-			Ack:  safe_packets.NewSafeAck(1),
-			Addr: test_helpers.MakeMockAddr("fakenet", "a"),
-		}
-	}()
+	manager.SendAckToReadSession(&safety_filter.IncomingSafeAck{
+		Ack:  safe_packets.NewSafeAck(1),
+		Addr: test_helpers.MakeMockAddr("fakenet", "a"),
+	})
 
 	runtime.Gosched()
 
@@ -122,12 +125,10 @@ func TestIncomingAckIsRoutedToCorrectSession(t *testing.T) {
 		t.Fatalf("Received incorrect data %v, expected %v", responseAgentA.MostRecentData(), expectedData)
 	}
 
-	go func() {
-		manager.Ack <- &safety_filter.IncomingSafeAck{
-			Ack:  safe_packets.NewSafeAck(1),
-			Addr: test_helpers.MakeMockAddr("fakenet", "b"),
-		}
-	}()
+	manager.SendAckToReadSession(&safety_filter.IncomingSafeAck{
+		Ack:  safe_packets.NewSafeAck(1),
+		Addr: test_helpers.MakeMockAddr("fakenet", "b"),
+	})
 
 	runtime.Gosched()
 
